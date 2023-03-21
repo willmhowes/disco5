ba536c18cd0393b6d43899f877634bf08bd65c3b
Will Howes <willmhowes@gmail.com>
2023-03-21 11:16:52 -0700

    improve StatusRegister code

--- src/computer/cpu.rs
+++ src/computer/cpu.rs
@@ -59,46 +59,47 @@ pub struct StatusRegister {
     pub c: bool,
 }
 
+/// bitflag representation of the N flag
+const N: u8 = 0b1000_0000;
+/// bitflag representation of the V flag
+const V: u8 = 0b0100_0000;
+/// bitflag representation of the B flag
+const B: u8 = 0b0001_0000;
+/// bitflag representation of the D flag
+const D: u8 = 0b0000_1000;
+/// bitflag representation of the I flag
+const I: u8 = 0b0000_0100;
+/// bitflag representation of the Z flag
+const Z: u8 = 0b0000_0010;
+/// bitflag representation of the C flag
+const C: u8 = 0b0000_0001;
+
 impl StatusRegister {
     /// returns status register represented by an 8-bit number
     pub fn to_byte(&self) -> u8 {
         // unused flag is always set to 1
         let mut byte: u8 = 0b0010_0000;
 
-        let n: u8 = 0b1000_0000;
-        let v: u8 = 0b0100_0000;
-        let b: u8 = 0b0001_0000;
-        let d: u8 = 0b0000_1000;
-        let i: u8 = 0b0000_0100;
-        let z: u8 = 0b0000_0010;
-        let c: u8 = 0b0000_0001;
-        byte = if self.n == true { byte | n } else { byte };
-        byte = if self.v == true { byte | v } else { byte };
-        byte = if self.b == true { byte | b } else { byte };
-        byte = if self.d == true { byte | d } else { byte };
-        byte = if self.i == true { byte | i } else { byte };
-        byte = if self.z == true { byte | z } else { byte };
-        byte = if self.c == true { byte | c } else { byte };
+        byte = if self.n == true { byte | N } else { byte };
+        byte = if self.v == true { byte | V } else { byte };
+        byte = if self.b == true { byte | B } else { byte };
+        byte = if self.d == true { byte | D } else { byte };
+        byte = if self.i == true { byte | I } else { byte };
+        byte = if self.z == true { byte | Z } else { byte };
+        byte = if self.c == true { byte | C } else { byte };
 
         byte
     }
 
     /// sets status register using an 8-bit number
     pub fn set_from_byte(&mut self, p: u8) {
-        let n: u8 = 0b1000_0000;
-        let v: u8 = 0b0100_0000;
-        let b: u8 = 0b0001_0000;
-        let d: u8 = 0b0000_1000;
-        let i: u8 = 0b0000_0100;
-        let z: u8 = 0b0000_0010;
-        let c: u8 = 0b0000_0001;
-        self.n = if p & n == n { true } else { false };
-        self.v = if p & v == v { true } else { false };
-        self.b = if p & b == b { true } else { false };
-        self.d = if p & d == d { true } else { false };
-        self.i = if p & i == i { true } else { false };
-        self.z = if p & z == z { true } else { false };
-        self.c = if p & c == c { true } else { false };
+        self.n = if p & N == N { true } else { false };
+        self.v = if p & V == V { true } else { false };
+        self.b = if p & B == B { true } else { false };
+        self.d = if p & D == D { true } else { false };
+        self.i = if p & I == I { true } else { false };
+        self.z = if p & Z == Z { true } else { false };
+        self.c = if p & C == C { true } else { false };
     }
 }
 
